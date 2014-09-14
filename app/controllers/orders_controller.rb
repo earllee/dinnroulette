0d9feec0d59e214c750eb0741a6a3ee59ab64a08
Marvin Qian <fueledbymarvin@gmail.com>
2014-09-14 01:35:16 -0400

    Add minimum and maximum options

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,41 +4,41 @@ class OrdersController < ApplicationController
   respond_to :json
 
   def create
+    # check all params there
     errors = check_params
     if errors.length != 0
       render json: {danger: errors}
     else
-      dl = OrdrIn.delivery_list(order_params)
-      if dl.length == 0
+      # grab possible restaurants
+      initial_dl = OrdrIn.delivery_list(order_params)
+      if initial_dl.length == 0
         render json: {warning: "No delivery available."}
+      end
+      # filter out too expensive minimums
+      dl = []
+      initial_dl.each do |restaurant|
+        if order_params["max"].to_f > restaurant["mino"].to_f
+          dl << restaurant
+        end
+      end
+      if dl.length == 0
+        render json: {warning: "No delivery available below that maximum."}
       else
+        # grab random restaurant
         restaurant_summary = dl[Random.rand(dl.length)]
         restaurant = OrdrIn.restaurant(restaurant_summary["id"])
-        items = OrdrIn.findItems(restaurant["menu"])
+        items = OrdrIn.find_items(restaurant["menu"])
         if items.length == 0
           render json: {warning: "No items available."}
         else
-          tray = []
-          while subtotal(tray) < restaurant_summary["mino"] do
-            tray << items[Random.rand(items.length)]
-          end
+          min = restaurant_summary["mino"].to_f if restaurant_summary["mino"].to_f > order_params["min"].to_f else order_params["min"].to_f
+          tray = OrdrIn.create_meal(items, min, order_params["max"].to_f)
           order = OrdrIn.order(restaurant["restaurant_id"], tray, order_params)
           if order["_err"] == "1"
             render json: {danger: "Order failed."}
           else
-            purchased_hash = {}
-            tray.each do |item|
-              if purchased_hash[item["name"]]
-                purchased_hash[item["name"]]["quantity"] += 1
-              else
-                purchased_hash[item["name"]] = {"name" => item["name"], "price" => item["price"], "quantity" => 1}
-              end
-            end
-            purchased = []
-            purchased_hash.each do |k, v|
-              purchased << v
-            end
-            render json: purchased
+            receipt = OrdrIn.create_receipt(tray)
+            render json: receipt
           end
         end
       end
@@ -47,17 +47,9 @@ class OrdersController < ApplicationController
 
   private
 
-  def subtotal(items)
-    sum = 0.0
-    items.each do |item|
-      sum += item["price"].to_f
-    end
-    sum
-  end
-
   def check_params
     errors = []
-    ["first_name", "last_name", "addr", "city", "state", "zip", "phone", "em", "card_name", "card_number", "card_cvc", "card_expiry", "card_bill_addr", "card_bill_city", "card_bill_state", "card_bill_zip"].each do |param|
+    ["first_name", "last_name", "addr", "city", "state", "zip", "phone", "em", "card_name", "card_number", "card_cvc", "card_expiry", "card_bill_addr", "card_bill_city", "card_bill_state", "card_bill_zip", "min", "max"].each do |param|
       if !order_params[param]
         errors << "Missing #{param}."
       end
@@ -66,7 +58,7 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.permit(:first_name, :last_name, :addr, :city, :state, :zip, :phone, :em, :card_name, :card_number, :card_cvc, :card_expiry, :card_bill_addr, :card_bill_city, :card_bill_state, :card_bill_zip)
+    params.permit(:first_name, :last_name, :addr, :city, :state, :zip, :phone, :em, :card_name, :card_number, :card_cvc, :card_expiry, :card_bill_addr, :card_bill_city, :card_bill_state, :card_bill_zip, :min, :max)
   end
 
 end
--- lib/ordr_in.rb
+++ lib/ordr_in.rb
@@ -19,11 +19,11 @@ class OrdrIn
     self.base_uri 'https://o-test.ordr.in'
     options["tip"] = "0.00"
     options["delivery_date"] = "ASAP"
-    options["tray"] = tray
+    options["tray"] = build_tray(tray)
     post("/o/#{rid}", options).parsed_response
   end
 
-  def self.findItems(section)
+  def self.find_items(section)
     items = []
     section.each do |item|
       if item["is_orderable"] == "1"
@@ -31,13 +31,13 @@ class OrdrIn
           items << item
         end
       elsif item["children"]
-        items = items + findItems(item["children"])
+        items = items + find_items(item["children"])
       end
     end
     items
   end
 
-  def build_tray(tray)
+  def self.build_tray(tray)
     tray_hash = {}
     tray.each do |item|
       if tray_hash[item["id"]]
@@ -54,5 +54,70 @@ class OrdrIn
       str += "#{id}/#{count}"
     end
     str
+  end
+  
+  def self.create_receipt(tray)
+    purchased_hash = {}
+    tray.each do |item|
+      if purchased_hash[item["name"]]
+        purchased_hash[item["name"]]["quantity"] += 1
+      else
+        purchased_hash[item["name"]] = {"name" => item["name"], "price" => item["price"], "quantity" => 1}
+      end
+    end
+    purchased = []
+    purchased_hash.each do |k, v|
+      purchased << v
+    end
+    purchased
+  end
+
+  def self.create_meal(items, min, max)
+    meal = []
+    if items.all? { |item| item["price"].to_f > max }
+      return []
+    else
+      while subtotal(meal) < min do
+        total = subtotal(meal)
+        item = item_in_range(items, min - total, max - total)
+        if !item
+          meal << items[Random.rand(items.length)]
+        else
+          meal << item
+        end
+      end
+      while subtotal(meal) < max do
+        if Random.rand(2) == 0
+          return meal
+        end
+        total = subtotal(meal)
+        item = item_in_range(items, 0, max - total)
+        if !item
+          return meal
+        else
+          meal << item
+        end
+      end
+    end
+  end
+
+  def self.subtotal(items)
+    sum = 0.0
+    items.each do |item|
+      sum += item["price"].to_f
+    end
+    sum
+  end
+
+  def self.item_in_range(items, min, max)
+    possible = items.find_all do |item|
+      price = item["price"].to_f
+      price >= min && price <= max
+    end
+    if possible.length == 0
+      nil
+    else
+      possible[Random.rand(possible.length)]
+    end
   end
 end
