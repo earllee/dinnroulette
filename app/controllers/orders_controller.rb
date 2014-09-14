53a310d142f2f63e93aa2e5ef0e10fb7cdc11969
Marvin Qian <fueledbymarvin@gmail.com>
2014-09-14 02:02:32 -0400

    Add error handling if some restaurant doesn't offer things below max

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,41 +8,51 @@ class OrdersController < ApplicationController
     errors = check_params
     if errors.length != 0
       render json: {danger: errors}
-    else
-      # grab possible restaurants
-      initial_dl = OrdrIn.delivery_list(order_params)
-      if initial_dl.length == 0
-        render json: {warning: "No delivery available."}
-      end
-      # filter out too expensive minimums
-      dl = []
-      initial_dl.each do |restaurant|
-        if order_params["max"].to_f > restaurant["mino"].to_f
-          dl << restaurant
-        end
-      end
-      if dl.length == 0
-        render json: {warning: "No delivery available below that maximum."}
-      else
-        # grab random restaurant
-        restaurant_summary = dl[Random.rand(dl.length)]
-        restaurant = OrdrIn.restaurant(restaurant_summary["id"])
-        items = OrdrIn.find_items(restaurant["menu"])
-        if items.length == 0
-          render json: {warning: "No items available."}
-        else
-          min = restaurant_summary["mino"].to_f if restaurant_summary["mino"].to_f > order_params["min"].to_f else order_params["min"].to_f
-          tray = OrdrIn.create_meal(items, min, order_params["max"].to_f)
-          order = OrdrIn.order(restaurant["restaurant_id"], tray, order_params)
-          if order["_err"] == "1"
-            render json: {danger: "Order failed."}
-          else
-            receipt = OrdrIn.create_receipt(tray)
-            render json: receipt
-          end
-        end
+      return
+    end
+    # grab possible restaurants
+    initial_dl = OrdrIn.delivery_list(order_params)
+    if initial_dl.length == 0
+      render json: {warning: "No delivery available."}
+      return
+    end
+    # filter out too expensive minimums
+    dl = []
+    initial_dl.each do |restaurant|
+      if order_params["max"].to_f > restaurant["mino"].to_f
+        dl << restaurant
       end
     end
+    if dl.length == 0
+      render json: {warning: "No delivery available below that maximum."}
+      return
+    end
+    tray = []
+    tries = 0
+    while tray.length == 0 do
+      if tries > 5
+        render json: {warning: "Please raise your maximum."}
+        return
+      end
+      # grab random restaurant
+      restaurant_summary = dl[Random.rand(dl.length)]
+      restaurant = OrdrIn.restaurant(restaurant_summary["id"])
+      items = OrdrIn.find_items(restaurant["menu"])
+      if items.length == 0
+        render json: {warning: "No items available."}
+        return
+      end
+      min = (restaurant_summary["mino"].to_f > order_params["min"].to_f) ? restaurant_summary["mino"].to_f : order_params["min"].to_f
+      tray = OrdrIn.create_meal(items, min, order_params["max"].to_f)
+      tries += 1
+    end
+    order = OrdrIn.order(restaurant["restaurant_id"], tray, order_params)
+    if order["_err"] == "1"
+      render json: {danger: "Order failed."}
+      return
+    end
+    receipt = OrdrIn.create_receipt(tray)
+    render json: receipt
   end
 
   private
